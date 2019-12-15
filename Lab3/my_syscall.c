13c7cfd7bafb3f19a07dd1dc25e05debc04a4b18
hazxel <steven.b.zhou@foxmail.com>
2019-12-15 08:20:40 -0800

    v2p ok, reading /dev/mem not ok

--- Lab3/my_syscall.c
+++ Lab3/my_syscall.c
@@ -1,43 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <asm/page.h>
-#include <linux/dup_types.h>
+//#include <asm/page.h>
+//#include <linux/types.h>
+//#include <linux/dup_types.h>
+#include <fcntl.h>
+#include <errno.h>
 
-unsigned long v2p(int, unsigned long);
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdint.h>
+
+int v2p(unsigned long, unsigned long*);
 
 int main(void){
-	v2p(1292, 0);
+	extern int errno;
+	int a = 2;
 
-	int fd;
+
+	//convert the address
+	unsigned long vir = (unsigned long)&a;
+	unsigned long phy = 0;
+	if (v2p(vir, &phy) == -1) {
+		printf("convert error.\n");
+		return -1;
+	}
+	printf("the virtual  address is: 0x%08lX\nthe physical address is: 0x%08lX\n", vir, phy);
+
+
+	//print the pages
+	int fd = open("/dev/mem", O_RDONLY | O_SYNC);
 	char *rdbuf;
-	int i;
-	fd = open("/dev/mem",O_RDWR|O_SYNC);
-	if(fd < 0){
+	if (fd < 0){
 		printf("open /dev/mem failed.\n");
 		return -1;
 	}
-	read(fd,rdbuf,10);
-	for(i = 0;i < 10;i++)
-	{
+	long int pos;
+	if (lseek(fd, 1048574, SEEK_SET) == -1) {
+                printf("lseek error %d\n", errno);
+		close(fd);
+                return -1;
+        }
+	if (read(fd, rdbuf, 2) != 2) {
+		printf("read error %d\n", errno);
+		close(fd);
+		return -1;
+	}
+	int i;
+	for (i = 0; i < 1; i = i + 1) {
 		printf("mem[%d]:0x%02X\n",i,*(rdbuf + i) & 0xff);
 	}
+	//read(fd, rdbuf, phy + 1);
+	//printf("%d\n", *(rdbuf + phy) & 0xff);
+	
 	close(fd);
 	return 0;
 }
 
-unsigned long v2p(int pid, unsigned long va){
-	unsigned long pa = 0;
-	struct task_struct *pcb_tmp = NULL;
-        pgd_t *pgd_tmp = NULL;
-        pud_t *pud_tmp = NULL;
-        pmd_t *pmd_tmp = NULL;
-        pte_t *pte_tmp = NULL;
-	if(!(pcb_tmp = findTaskByPid(pid)))
-        {
-                printf("Can't find the task %d .\n",pid);
-                return 0;
+int v2p(unsigned long va, unsigned long *pa){
+	int pageSize = getpagesize();
+	unsigned long v_pageIndex = va / pageSize;
+	unsigned long v_offset = v_pageIndex * sizeof(uint64_t);
+	unsigned long page_offset = va % pageSize;
+	uint64_t item = 0;
+	printf(" ** virtual addr: 0x%lX\n"
+	       " ** page size: %d\n"
+	       " ** entry size: %d\n"
+	       " ** virtual page index: %ld\n"
+	       " ** in-page offset: %ld\n",
+	       va, pageSize, sizeof(uint64_t), v_pageIndex, page_offset);	
+
+	int fd = open("/proc/self/pagemap", O_RDONLY);
+	if (fd < 0) {
+                printf("open /proc/self/pagemap failed.\n");
+                return -1;
         }
-	printf("pgd = 0x%p\n",pcb_tmp->mm->pgd);
+	if (lseek(fd, v_offset, SEEK_SET) == -1) {
+		printf("lseek error\n");
+		return -1;
+	}
+	if (read(fd, &item, sizeof(uint64_t)) != sizeof(uint64_t)) {
+		printf("read item error\n");
+		return -1;
+	}
+	if ((((uint64_t)1 << 63) & item) == 0) {
+		printf("page present is 0\n");
+		return -1;
+	}
+	
+	printf(" ** the No.%ld item in page map is: 0x%016llX\n",v_pageIndex, item);
+	
+	uint64_t p_pageIndex = ((((uint64_t) 1 << 55) - 1) & item);
+	*pa =  (p_pageIndex * pageSize) + page_offset;
+	printf(" ** physical page index: %lld\n", p_pageIndex);
+	
+	//struct task_struct *pcb_tmp = NULL;
+        //pgd_t *pgd_tmp = NULL;
+        //pud_t *pud_tmp = NULL;
+        //pmd_t *pmd_tmp = NULL;
+        //pte_t *pte_tmp = NULL;
+	//if(!(pcb_tmp = findTaskByPid(pid)))
+        //{
+        //        printf("Can't find the task %d .\n",pid);
+        //        return 0;
+        //}
+	//printf("pgd = 0x%p\n",pcb_tmp->mm->pgd);
+	//struct task_struct *curr= get_current();     
+	close(fd);
+	return 0;
 }
 
 
